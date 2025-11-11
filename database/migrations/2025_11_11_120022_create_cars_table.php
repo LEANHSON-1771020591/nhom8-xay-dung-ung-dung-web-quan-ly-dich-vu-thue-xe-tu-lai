9273fd89f7f446b45aa4ea463447575fd6908a07
LE ANH SON <1771020591@dnu.edu.vn>
2025-11-11 19:17:21 +0700

    add Car

--- database/migrations/2025_11_11_120022_create_cars_table.php
+++ database/migrations/2025_11_11_120022_create_cars_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,18 @@ return new class extends Migration
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("address");
+            $table->string("location");
+            $table->string("price");
+            $table->string("images");
+            $table->string("desc");
+            $table->string("model");
+            $table->string("transmission");
+            $table->string("fuel");
+            $table->string("consumed");
+            $table->int("trip");
+            $table->int("seat");
+            $table->int("owner_id");
         });
     }
 
