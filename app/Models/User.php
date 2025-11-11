aef6d8e677f03904c4c33bdc5f7cc64a63a23e99
LE ANH SON <1771020591@dnu.edu.vn>
2025-11-11 19:37:08 +0700

    modife User

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,11 +17,7 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guard = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -30,7 +26,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
