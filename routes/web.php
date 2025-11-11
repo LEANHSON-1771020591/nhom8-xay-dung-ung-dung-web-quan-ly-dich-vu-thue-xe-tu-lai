ea01ac690dd90d896e497f1c5039a9418c1e13c6
LE ANH SON <1771020591@dnu.edu.vn>
2025-11-11 21:44:21 +0700

    Add main page route,index category route and show car route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Car;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $cars = Car::take(8)->get();
+    return view('index', compact('cars'));
+});
+
+Route::get("/filter/{slug}", function ($slug) {
+    $cars = Car::where("location", $slug)->get();
+    return view("category.index", compact(["slug", "cars"]));
+});
+
+Route::get("/car/{slug}", function ($slug) {
+    $car = Car::where("slug", $slug)->first();
+    return view("car.show", compact("car"));
 });
